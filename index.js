29595b272357c1facf8174451630f7393fdcbdd0
sohanemon <m.sohanemon@gmail.com>
2022-10-27 13:15:17 +0600

    faq added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 const courses = require("./data/courses.json");
+const faq = require("./data/faq.json");
 app.get("/", (req, res) => {
   res.send("15minutelearning server is running...");
 });
@@ -12,6 +13,9 @@ app.get("/courses", (req, res) => {
 app.get("/courses/:id", (req, res) => {
   const course = courses.find((el) => el.id === req.params.id);
   res.send(course);
+});
+app.get("/faq", (req, res) => {
+  res.send(faq);
 });
 
 app.listen(5000);
